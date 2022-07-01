7044a87cc670562a39f7748f003bf1f0c06f2ecf
r2ff <reni.gate@gmail.com>
2022-07-01 17:40:07 +0500

    second commit

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     button_is_visible = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket").is_displayed()
     assert button_is_visible is True
